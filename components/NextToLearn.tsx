9b520b7f142648bb0571de45eb0d86637a7ae1b7
Donato Monzon <spartanhalokitty@gmail.com>
2023-01-08 17:54:57 -0600

    fix: remove  unused code

--- components/NextToLearn.tsx
+++ components/NextToLearn.tsx
@@ -37,53 +37,6 @@ export const NextToLearn = () => {
           </div>
 
         </div>
-
-        {/* <div className="custom-container">
-          <div className='flex justify-center pt-8 pb-4'>
-            <IconWithText title="Rust" iconURL={'/icons/rust.svg'} hasBackground/>
-
-          </div>
-          <p className="paragraph">
-          Rust es un lenguaje que ha comenzado a crecer muy rápidamente. Prueba de ello son las nuevas herramientas y tecnologías que han comenzado a surgir ultimamente. Tiene una gran comunidad de la cual quiero formar parte.
-          </p>
-        </div>
-
-        <div className="custom-container">
-          <div className='flex justify-center py-8'>
-            <IconWithText title="React Native" iconURL={'/icons/react-native.svg'} hasBackground/>
-          </div>
-          <p className="paragraph">
-          Quiero llevar el desarrollo de aplicaciones hechas en React al siguiente nivel y a un nuevo terreno. Esto se puede lograr con React Native para la creación de aplicaciones móviles multiplataforma. 
-          </p>
-        </div>
-
-        <div className="custom-container">
-          <div className='flex justify-center py-8'>
-            <IconWithText title="Dart" iconURL={ '/icons/dart.svg' } />
-            <IconWithText title="Flutter" iconURL={ '/icons/flutter.svg' }/>
-          </div>
-          <p className="paragraph">
-          Me interesa en un futuro a aprender a crear aplicaciones moviles. Dart y Flutter son un lenguaje y un framework que sirven para crear aplicaciones móviles multiplataforma y actualmente son una excelente opción para adentrarme al mundo del desarrollo móvil.
-          </p>
-        </div>
-
-        <div className="custom-container">
-          <div className='flex justify-center py-8'>
-            <IconWithText title="VueJS" iconURL={ '/icons/vue.svg' } />
-          </div>
-          <p className="paragraph">
-          VueJS es un framework de JavaScript que tiene una gran comunidad y un ecosistema que compite con React para la creación de aplicaciones web. Combina características de React y Angular y según algunos desarrolladores es fácil de aprender.
-          </p>
-        </div>
-
-        <div className="custom-container">
-          <div className='flex justify-center py-8'>
-            <IconWithText title="NestJS" iconURL={ '/icons/nestjs.svg' } />
-          </div>
-          <p className="paragraph">
-            NestJS es un framework para aplicaciones del servidor, basado en Express y con soporte incluido para TypeScript.
-          </p>
-        </div> */}
       </div>
     </div>
   )
